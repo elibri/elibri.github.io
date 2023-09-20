64ac022c261d961a9c758a6688697ec65863bda2
Tomek Meka <tomek@elibri.com.pl>
2023-09-20 16:29:02 +0200

    doc update

--- elibriPHP/search/classes_2.js
+++ elibriPHP/search/classes_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['firstnodevalue_558',['FirstNodeValue',['../classFirstNodeValue.html',1,'']]]
+  ['firstnodevalue_559',['FirstNodeValue',['../classFirstNodeValue.html',1,'']]]
 ];
